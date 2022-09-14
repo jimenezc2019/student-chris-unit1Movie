13a397de071fcb1cbbed35c2e2cb2c8df1f91726
jimenezc2019 <jimenezc2019@fau.edu>
2022-09-14 07:36:49 -0400

    Unit 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Christian Jimenez on 9/11/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
---
+++ Flix/MoviesDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MoviesDetailViewController.swift
+//  Flix
+//
+//  Created by Christian Jimenez on 9/11/22.
+//
+
+import UIKit
+import AlamofireImage
+class MoviesDetailViewController: UIViewController {
+
+    var movie: [String: Any]!
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+    
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        posterView.af_setImage(withURL: posterURL!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af_setImage(withURL: backdropURL!)
+    }
+        
+
+}
